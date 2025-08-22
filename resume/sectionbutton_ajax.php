f18699f6a69e6abcc90eaf3ba21cf769ab8f4450
Rudraksh Batra <38044398+rudykartson@users.noreply.github.com>
2025-08-22 12:32:48 +0530

    Update sectionbutton_ajax.php

--- resume/sectionbutton_ajax.php
+++ resume/sectionbutton_ajax.php
@@ -1,4 +1,11 @@
 <?php
+
+/**
+ * @package     local_resume
+ * @copyright   2025 Rudraksh Batra <batra.rudraksh@gmail.com>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
 require_once(__DIR__.'/../../config.php');
 require_login();
 header('Content-Type: text/html; charset=utf-8');
@@ -8,4 +15,5 @@ $sectionid = required_param('sectionid', PARAM_INT);
 
 require_once($CFG->dirroot . '/local/resume/lib.php');
 echo local_resume_render_resume_button($courseid, $sectionid);
+
 exit;
