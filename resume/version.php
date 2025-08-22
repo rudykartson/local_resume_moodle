c134be7b897d074732ee31745405372b366fd76f
Rudraksh Batra <38044398+rudykartson@users.noreply.github.com>
2025-08-22 12:31:40 +0530

    Update version.php

--- resume/version.php
+++ resume/version.php
@@ -1,8 +1,16 @@
 <?php
+
+/**
+ * @package     local_resume
+ * @copyright   2025 Rudraksh Batra <batra.rudraksh@gmail.com>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_resume';
 $plugin->version   = 2025071100;
 $plugin->requires  = 2023061200; // Moodle 4.3 (2023061200) minimum.
 $plugin->maturity  = MATURITY_STABLE;
+
 $plugin->release   = '1.0.0';
